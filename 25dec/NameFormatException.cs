b4f2480a545a79d4076fe0c068c2e4892d542250
Shukufa62 <119412605+Shukufa62@users.noreply.github.com>
2022-12-25 21:40:28 +0400

    Add files via upload

---
+++ 25dec/NameFormatException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace _25dec
+{
+	public class NameFormatException:Exception
+	{
+        public NameFormatException(string message) : base(message)
+        {
+
+        }
+    }
+}
+
